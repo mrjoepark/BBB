266d86446991e622cb32257dd2f97cbaa6fd3fd5
mrjoepark <joe.park20@gmail.com>
2017-04-15 13:58:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from xml.dom import minidom
 import urllib2
 from time import sleep 
 
-while True:
+#get gamday URL
+gameday=raw_input("Please paste the URL of the game you wish to track")
+str(gameday)
 
+while True:
+    
     #get the xml  Use boxscore.xml or linescore.xml 
-    html = urllib2.urlopen('http://gd2.mlb.com/components/game/mlb/year_2017/month_04/day_05/gid_2017_04_05_miamlb_wasmlb_1/linescore.xml')
+    html = urllib2.urlopen(gameday)
 
     #parse the xml
     xmldoc=minidom.parse(html)
@@ -18,12 +22,9 @@ while True:
     game_index=game[0]
 
     #access xml attribute using dictionary syntax
-    pbp=game_index.attributes['venue']
     current_inning=game_index.attributes['inning']
-   
-
+    
     print str(current_inning.value)
-    print str(pbp.value)
 
     sleep(60)
     del html
