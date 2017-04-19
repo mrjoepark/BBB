3bfe11548603990b54f4c63d793e7fece69aed0d
mrjoepark <joe.park20@gmail.com>
2017-04-19 12:47:43 -0500

    Add files via upload
    messing around

--- main.py
+++ main.py
@@ -6,15 +6,53 @@ from time import sleep
 #gameday=raw_input("Please paste the URL of the game you wish to track:  ")
 #str(gameday)
 
+#get the xm. Use linescore.xml 
+html=urllib2.urlopen('http://gd2.mlb.com/components/game/mlb/year_2017/month_04/day_18/gid_2017_04_18_sfnmlb_kcamlb_1/linescore.xml')
+
+#parse the xml
+xmldoc=minidom.parse(html)
+
+#get the xml tag and access it using dictionary syntax
+game=xmldoc.getElementsByTagName('game')
+
+#can access element hierarchy using dictionary index
+game_index=game[0]
+
+#access xml attribute using dictionary syntax
+current_inning=game_index.attributes['inning']
+top_bottom=game_index.attributes['inning_state']
+home_score=game_index.attributes['home_team_runs']
+away_score=game_index.attributes['away_team_runs']
+home_team=game_index.attributes['home_team_name']
+away_team=game_index.attributes['away_team_name']
+#get values and turn the attirbutes into a int and use format to convert to binary
+int_current_inning=int(current_inning.value)
+str_top_bottom=str(top_bottom.value)
+int_home_score=int(home_score.value)
+int_away_score=int(away_score.value)
+str_home_team=home_team.value
+str_away_team=away_team.value 
+
+#format(value, '04' lead spaces, b=binary)
+bin_current_inning=format(int_current_inning, '04b')
+bin_home_score=format(int_home_score, '05b')
+bin_away_score=format(int_away_score, '05b')
+print bin_current_inning
+print bin_home_score
+print bin_away_score 
+bin_current_inning=list(bin_current_inning)
+bin_home_score=list(bin_home_score)
+bin_away_score=list(bin_away_score) 
 
 #define GPIO on/off functions here
+#figure out runner on base status pattern 
 def score():
     print str_away_team, int_away_score
     print str_home_team, int_home_score
 
 def topOrBottom():
     print str_top_bottom 
-    if top_bottom=='Top':
+    if str_top_bottom=='Top':
         print 'top on'
     else:
         print 'bottom on' 
@@ -65,7 +103,7 @@ def GPIO_home_score():
 
 def GPIO_away_score():
     print 'away score binary'
-    #turn score lights on or off from bin_home_score 
+    #turn score lights on or off from bin_away_score 
     if bin_away_score[0]== '1':
         print ' first on'
     else:
@@ -89,44 +127,7 @@ def GPIO_away_score():
         
 #loop 
 while True:
-    #get the xml  Use boxscore.xml or linescore.xml 
-    html=urllib2.urlopen('http://gd2.mlb.com/components/game/mlb/year_2017/month_04/day_18/gid_2017_04_18_sfnmlb_kcamlb_1/linescore.xml')
-
-    #parse the xml
-    xmldoc=minidom.parse(html)
-
-    #get the xml tag and access it using dictionary syntax
-    game=xmldoc.getElementsByTagName('game')
-
-    #can access element hierarchy using dictionary index
-    game_index=game[0]
-
-    #access xml attribute using dictionary syntax
-    current_inning=game_index.attributes['inning']
-    top_bottom=game_index.attributes['inning_state']
-    home_score=game_index.attributes['home_team_runs']
-    away_score=game_index.attributes['away_team_runs']
-    home_team=game_index.attributes['home_team_name']
-    away_team=game_index.attributes['away_team_name']
-    #get values and turn the attirbutes into a int and use format to convert to binary
-    int_current_inning=int(current_inning.value)
-    str_top_bottom=str(top_bottom.value)
-    int_home_score=int(home_score.value)
-    int_away_score=int(away_score.value)
-    str_home_team=home_team.value
-    str_away_team=away_team.value 
-    
-    #format(value, '04' lead spaces, b=binary
-    bin_current_inning=format(int_current_inning, '04b')
-    bin_home_score=format(int_home_score, '05b')
-    bin_away_score=format(int_away_score, '05b')
-    print bin_current_inning
-    print bin_home_score
-    print bin_away_score 
-    bin_current_inning=list(bin_current_inning)
-    bin_home_score=list(bin_home_score)
-    bin_away_score=list(bin_away_score) 
-
+ 
     #GPIO functions
     score()
     topOrBottom()
@@ -136,3 +137,4 @@ while True:
     
     sleep(60)
     del html
+     
